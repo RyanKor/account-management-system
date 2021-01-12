ad075bfac912cf509894b875a845348ad8c6a636
RyanKor <equuse3144@gmail.com>
2021-01-12 09:24:30 +0700

    직원 추가하기 기능 추가. 나동빈의 리액트 프로그래밍 참고

---
+++ client/src/component/employee/AddEmployee.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import {post} from 'axios'
+
+
+class AddEmployee extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            file : null,
+            userName : '',
+            birthday : '',
+            gender : '',
+            job : '',
+            fileName : ''
+        }
+    }
+    handleFormSubmit = (e)=>{
+        e.preventDefault()
+        this.addCustomer()
+        .then(response=>{
+            console.log(response.data)
+            this.props.stateRefresh() //서버로부터 응답을 받고 난 뒤에 state를 refresh한다.
+        })
+        this.setState({
+            file : null,
+            userName : '',
+            birthday : '',
+            gender : '',
+            job : '',
+            fileName : ''
+        })
+
+    }
+    handleFileChange = (e)=>{
+        this.setState({
+            file : e.target.files[0], // input 에 입력된 첫번째 파일만!
+            fileName: e.target.value
+
+        })
+    }
+    handleValueChange = (e)=>{
+        let nextState = {}
+        nextState[e.target.name] = e.target.value
+        this.setState(nextState)
+    }
+
+    addCustomer = ()=>{
+        const url = '/api/customers'
+        const formData = new FormData()
+        formData.append('image', this.state.file)
+        formData.append('name', this.state.userName)
+        formData.append('birthday', this.state.birthday)
+        formData.append('gender', this.state.gender)
+        formData.append('job', this.state.job)
+
+        const config = {
+            headers : {
+                "content-type" : "multipart/form-data"
+            }
+        }
+        return post(url, formData, config)
+    }
+    render(){
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h1>Add Employee</h1>
+                Profile Image : 
+                <input 
+                type="file" 
+                name="file" 
+                file={this.state.file} 
+                value={this.state.fileName}
+                onChange={this.handleFileChange} /> <br />
+                Name : 
+                <input 
+                type="text" 
+                name="userName" 
+                value={this.state.userName} 
+                onChange={this.handleValueChange}/> <br />
+                Birthday : 
+                <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange} /> <br />
+                Gender : 
+                <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange} /> <br />
+                Job : 
+                <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange} /> <br />
+                <button type="submit">Add Customer</button>
+            </form>
+        )
+    }
+}
+
+export default AddEmployee
